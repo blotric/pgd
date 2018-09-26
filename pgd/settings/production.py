dd4ad4ffe1fd31c3153e70bf4f721d4ea5ec0f09
Boštjan Lotrič <blotric@gmail.com>
2018-09-26 21:40:53 +0200

    Add email settings

--- pgd/settings/production.py
+++ pgd/settings/production.py
@@ -25,6 +25,24 @@ DATABASES = {
 
 ALLOWED_HOSTS = ['pgd.pollvortex.com']
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = 'ubuntu-pgd'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
+DEFAULT_FROM_EMAIL = 'PGD Železniki <info@pgd-zelezniki.si>'
+
+# # EMAIL_USE_TLS = True
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = '52.59.193.205'
+# # EMAIL_HOST_PASSWORD = 'bpdcuqqkibaexwvc'
+# EMAIL_HOST_USER = 'mail@cargox.io'
+# EMAIL_PORT = 1587
+
+# EMAIL_SUBJECT_PREFIX = '[CargoX API] '
+
+
 try:
     from .local import *
 except ImportError:
