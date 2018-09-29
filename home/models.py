3f41e8c5c1490af3d72dea2d9f96af0ee49a5e9c
Boštjan Lotrič <blotric@gmail.com>
2018-09-29 17:59:09 +0200

    Add support for gallery images on home page

--- home/models.py
+++ home/models.py
@@ -1,7 +1,26 @@
 from django.db import models
 
-from wagtail.core.models import Page
+from wagtail.core.models import Page, Orderable
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
+from wagtail.images.edit_handlers import ImageChooserPanel
+
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
 
 
 class HomePage(Page):
-    pass
+    content_panels = Page.content_panels + [
+        InlinePanel('gallery_images', label="Gallery images"),
+    ]
+
+
+class HomePageGalleryImage(Orderable):
+    page = ParentalKey(HomePage, on_delete=models.CASCADE, related_name='gallery_images')
+    image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
+    )
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        ImageChooserPanel('image'),
+        FieldPanel('caption'),
+    ]
